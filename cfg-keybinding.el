56b25c09191704bfc2009849448d5bab663b782d
kimim <m@kimi.im>
2015-09-25 21:21:15 +0800

    add ace-isearch, ace-window, helm

--- cfg-keybinding.el
+++ cfg-keybinding.el
@@ -59,6 +59,7 @@
 (global-set-key "\C-c\C-x\C-i" 'org-clock-in)
 (global-set-key "\C-c\C-x\C-o" 'org-clock-out)
 (global-set-key [?\C-c ?\C-/] 'comment-or-uncomment-region)
+(define-key global-map (kbd "C-c SPC") 'ace-jump-mode)
 
 (define-key global-map (kbd "RET") 'newline-and-indent)
 
