ea7e6d034bddba6d946d95776409b49a161a9499
kimim <m@kimi.im>
2016-10-11 21:17:31 +0800

    optimize the init.el with parameter to org-babel-load-file

--- init.el
+++ init.el
@@ -1,10 +1,8 @@
 (require 'org)
 
-;; load the latest configuration
 (if (>
      (nth 1 (nth 5 (file-attributes "~/kimim-emacs/README.org")))
      (nth 1 (nth 5 (file-attributes "~/kimim-emacs/README.el"))))
-    (progn
-      (org-babel-load-file "~/kimim-emacs/README.org")
-      (byte-compile-file "~/kimim-emacs/README.el"))
+    ;; load and byte compile the latest configuration
+    (org-babel-load-file "~/kimim-emacs/README.org" t)
   (load-file  "~/kimim-emacs/README.el"))
