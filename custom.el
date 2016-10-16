03262528b5b81ba04c97b31fac6e8124d88117ba
kimim <m@kimi.im>
2016-10-16 16:44:38 +0800

    reorganize config

--- custom.el
+++ custom.el
@@ -8,9 +8,6 @@
  '(ansi-color-names-vector
    (vector "#c5c8c6" "#cc6666" "#b5bd68" "#f0c674" "#81a2be" "#b294bb" "#8abeb7" "#373b41"))
  '(custom-enabled-themes (quote (sanityinc-tomorrow-blue)))
- '(custom-safe-themes
-   (quote
-    ("82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" default)))
  '(ecb-compilation-buffer-names
    (quote
     (("*Calculator*")
@@ -31,8 +28,8 @@
      ("Calendar")
      ("\\*helm.*\\*" . t)
      ("\\*helm-mode-.*\\*" . t))))
- '(ecb-compile-window-width (quote edit-window))
- '(ecb-layout-name "left-kimi0")
+ '(ecb-compile-window-width (quote edit-window) t)
+ '(ecb-layout-name "left-kimi0" t)
  '(ecb-layout-window-sizes
    (quote
     (("left-kimi0"
@@ -56,13 +53,13 @@
     ((default ecb-tag-visit-smart-tag-start ecb-tag-visit-highlight-tag-header ecb-tag-visit-recenter-top)
      (java-mode ecb-tag-visit-goto-doc-start)
      (jde-mode ecb-tag-visit-goto-doc-start))))
- '(ecb-tip-of-the-day nil)
+ '(ecb-tip-of-the-day nil t)
  '(fci-rule-color "#373b41")
  '(fill-column 80)
  '(ggtags-find-tag-hook (quote (recenter)))
  '(package-selected-packages
    (quote
-    (path-headerline-mode auto-highlight-symbol autopair paredit swift-mode mocker which-key window-numbering volatile-highlights use-package pallet package-build shut-up epl git commander f s cask markdown-mode ggtags org jedi col-highlight ox-ioslide company history drag-stuff whole-line-or-region w32-registry ox-html5slide htmlize graphviz-dot-mode google-c-style gnuplot find-file-in-repository fill-column-indicator everything elpy ecb cygwin-mount color-theme-github browse-kill-ring bbdb auto-complete-clang-async auto-complete-clang ace-window ace-isearch ac-clang)))
+    (company-irony irony color-theme path-headerline-mode auto-highlight-symbol autopair paredit swift-mode mocker which-key window-numbering volatile-highlights use-package pallet package-build shut-up epl git commander f s cask markdown-mode ggtags org jedi col-highlight ox-ioslide company history drag-stuff whole-line-or-region w32-registry ox-html5slide htmlize graphviz-dot-mode google-c-style gnuplot find-file-in-repository fill-column-indicator everything elpy ecb cygwin-mount color-theme-github browse-kill-ring bbdb auto-complete-clang-async auto-complete-clang ace-window ace-isearch ac-clang)))
  '(python-shell-interpreter "C:\\Python35\\python")
  '(safe-local-variable-values
    (quote
--- emacs.bat
+++ emacs.bat
@@ -1,4 +1,4 @@
 :: set home path for emacs
 set HOME=C:\Users\kimim\home
 :: execute emacs with debug enable
-C:\Users\kimim\kimikit\emacs\bin\runemacs.exe --debug-init
+C:\kimikit\emacs\bin\runemacs.exe --debug-init
