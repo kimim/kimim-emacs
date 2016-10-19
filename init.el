92187451b72248735a2f6ce90cfd0da69e2348ec
kimim <m@kimi.im>
2016-10-19 20:56:58 +0800

    check file-exist-p of README.el in case no .el file generated.

--- init.el
+++ init.el
@@ -1,10 +1,15 @@
-(require 'org)
+(setq readme-org "~/kimim-emacs/README.org")
+(setq readme-el "~/kimim-emacs/README.el")
 
-(if (>
-     (nth 1 (nth 5 (file-attributes "~/kimim-emacs/README.org")))
-     (nth 1 (nth 5 (file-attributes "~/kimim-emacs/README.el"))))
+(if (or
+     ;; README.el not generated
+     (not (file-exists-p readme-el))
+     ;; or README.org newer than README.el
+     (>
+      (nth 1 (nth 5 (file-attributes readme-org)))
+      (nth 1 (nth 5 (file-attributes readme-el)))))
     ;; load and byte compile the latest configuration
     (progn
-      (org-babel-load-file "~/kimim-emacs/README.org" t)
-      (load-file  "~/kimim-emacs/README.el"))
-  (load-file  "~/kimim-emacs/README.el"))
+      (org-babel-load-file readme-org t)
+      (load-file readme-el))
+  (load-file readme-el))
