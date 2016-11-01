2479921b1e0b77eb5f3e03118331af7c9c875afb
kimim <m@kimi.im>
2016-11-01 13:09:40 +0800

    add counsel-pt and open-in-external-app func

--- site-lisp/kimim.el
+++ site-lisp/kimim.el
@@ -344,4 +344,29 @@
           (not (member (current-buffer) deft-auto-save-buffers)))
       (delete-trailing-whitespace)))
 
+(defun kimim/open-in-external-app (&optional file)
+  "Open the current file or dired marked files in external app.
+The app is chosen from your OS's preference.
+copy from xah lee: http://ergoemacs.org/emacs/emacs_dired_open_file_in_ext_apps.html"
+  (interactive)
+  (let ( doIt
+         (myFileList
+          (cond
+           ((string-equal major-mode "dired-mode") (dired-get-marked-files))
+           ((not file) (list (buffer-file-name)))
+           (file (list file)))))
+
+    (setq doIt (if (<= (length myFileList) 5)
+                   t
+                 (y-or-n-p "Open more than 5 files? ")))
+
+    (when doIt
+      (cond
+       ((string-equal system-type "windows-nt")
+        (mapc (lambda (fPath) (w32-shell-execute "open" (replace-regexp-in-string "/" "\\" fPath t t)) ) myFileList))
+       ((string-equal system-type "darwin")
+        (mapc (lambda (fPath) (shell-command (format "open \"%s\"" fPath)))  myFileList))
+       ((string-equal system-type "gnu/linux")
+        (mapc (lambda (fPath) (let ((process-connection-type nil)) (start-process "" nil "xdg-open" fPath)) ) myFileList))))))
+
 (provide 'kimim)
