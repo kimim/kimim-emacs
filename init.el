8b1c424445981c9b86572e90069455e54241dcbe
kimim <m@kimi.im>
2017-01-01 15:44:50 +0800

    fix file modification compare routine

--- init.el
+++ init.el
@@ -1,13 +1,19 @@
 (setq readme-org (concat kimim/path-kimim-emacs "README.org"))
 (setq readme-el (concat kimim/path-kimim-emacs "README.el"))
 
+(defun kimim/last-modify-time (file-name)
+  "get the last modification time of specified file"
+  (+ (* 65536 (nth 0 (nth 5 (file-attributes file-name))))
+     (nth 1 (nth 5 (file-attributes file-name)))))
+
+
 (if (or
      ;; README.el not generated
      (not (file-exists-p readme-el))
      ;; or README.org newer than README.el
      (>
-      (nth 1 (nth 5 (file-attributes readme-org)))
-      (nth 1 (nth 5 (file-attributes readme-el)))))
+      (kimim/last-modify-time readme-org)
+      (kimim/last-modify-time readme-el)))
     ;; load and byte compile the latest configuration
     (progn
       (org-babel-load-file readme-org t)
