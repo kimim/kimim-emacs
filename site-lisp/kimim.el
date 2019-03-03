f34029c091ba4f661b99c29ee37f7d9b71f005ea
kimim <kimi.im@outlook.com>
2019-03-03 16:34:47 +0800

    get rid of "\\" in windows environment when attach files

--- site-lisp/kimim.el
+++ site-lisp/kimim.el
@@ -422,7 +422,11 @@ copy from xah lee: http://ergoemacs.org/emacs/emacs_dired_open_file_in_ext_apps.
   (let ((file-list (x-get-clipboard)))
     (save-excursion
       (end-of-buffer)
-      (mapc '(lambda (item) (insert (concat "<#part filename=\"" item "\" disposition=attachment><#/part>\n")))
+      (mapc '(lambda (item)
+               (insert (concat "<#part filename=\""
+                               ;; remove "\\" in Windows environment
+                               (replace-regexp-in-string (regexp-quote "\\") "/" item)
+                               "\" disposition=attachment><#/part>\n")))
             (split-string file-list "\n" t)))))
 
 (provide 'kimim)
