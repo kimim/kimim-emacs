b54bbefcb5d3e7619a428a8cf12f016e011d884a
kimim <kimi.im@outlook.com>
2021-04-13 20:50:23 +0800

    in order to let buffer-id follow color change, just remove bgcolor

--- site-lisp/kimim-night-theme.el
+++ site-lisp/kimim-night-theme.el
@@ -107,7 +107,7 @@
             (ido-subdir :weight bold)
             (mode-line :foreground "white" :background "RoyalBlue4" :box nil :height 0.85)
             (mode-line-inactive :foreground "light blue" :background "#101d3c" :box nil :height 0.85)
-            (mode-line-buffer-id :foreground "light blue" :background "RoyalBlue4" :box nil :weight bold)
+            (mode-line-buffer-id :foreground "light blue" :box nil :weight bold)
             (which-func :foreground ,kimim-night-builtin)
             (minibuffer-prompt :foreground "light blue")
             (hl-line :background "gray12")
